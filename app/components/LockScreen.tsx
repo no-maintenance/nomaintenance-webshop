86f9d4d836d1271a6dbbafea988ca565efdf1a43
therealchrisrock <therealchrisrock@proton.me>
2024-07-08 12:20:42 -0400

    update lock screen

--- app/components/LockScreen.tsx
+++ app/components/LockScreen.tsx
@@ -108,7 +108,7 @@ export function LockScreen({
         </div>
         <div
           className={
-            'absolute pt-16 lg:pt-20 xl:pt-24  md:pr-10 md:right-0 md:top-0 right-1/2 translate-x-1/2 md:-translate-x-0 bottom-28 text-white text-mid max-w-full px-gutter w-full md:w-auto md:text-left text-center'
+            'absolute pt-16 lg:pt-20 xl:pt-24  md:pr-10 md:right-0 md:top-0 right-1/2 translate-x-1/2 md:-translate-x-0 bottom-36 text-white text-mid max-w-full px-gutter w-full md:w-auto md:text-left text-center'
           }
         >
           <h1 className={'text-heading uppercase mt-2 block font-semibold'}>
