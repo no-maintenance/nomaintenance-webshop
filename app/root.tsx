b12988a9011ceafa0a15b33ce913d22d29152f1a
therealchrisrock <therealchrisrock@proton.me>
2024-07-08 13:32:15 -0400

    update lock screen

--- app/root.tsx
+++ app/root.tsx
@@ -219,7 +219,7 @@ export function Document({
         <meta charSet="utf-8" />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,viewport-fit=cover"
+          content="width=device-width,initial-scale=1,maximum-scale=1,viewport-fit=cover"
         />
         <meta name="msvalidate.01" content="A352E6A0AF9A652267361BBB572B8468" />
         {allowIndexing ? null : (
